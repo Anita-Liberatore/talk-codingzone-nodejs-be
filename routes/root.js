e9a75d6b230cfd49d1addfa6ea865704ea4d4cb4
Anita Liberatore <anitaliberatore@hotmail.com>
2022-12-13 21:47:18 +0100

    done

--- routes/root.js
+++ routes/root.js
@@ -4,21 +4,39 @@ const got = require('got')
 module.exports = async function (fastify, opts) {
 
   fastify.get('/user', async function (request, reply) {
-    const users = await got(`https://dummyjson.com/users`).json()
-    return users
+    const users = await got(`https://randomuser.me/api`).json()
+
+    const result = users['results'].map( x => {
+      
+      return {gender: x.gender};
+      }
+    )
+
+    
+
+    return reply.send(result)
   })
 
   fastify.get('/user/:id', async function (request, reply) {
     const { id } = request.params
     const user = await got(`https://dummyjson.com/users/${id}`).json()
-    return user;
+
+    return {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName
+    }
 
   })
 
   fastify.get('/user/search/:name', async function (request, reply) {
     const { name } = request.params
-    const user = await got(`https://dummyjson.com/users/search?q=${name}`).json()
-    return user;
+    const users = await got(`https://dummyjson.com/users/search?q=${name}`).json()
+
+
+    reply.send(users['users'].reduce((acc, val) => {
+      acc.push(val['id'])
+    }))
 
   })
 }
